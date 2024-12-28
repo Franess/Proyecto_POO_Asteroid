2c3a0ef59732d06475a8c324a084aadb1b8830a8
Sun <glassedsun@gmail.com>
2024-12-28 20:06:30 -0300

    implementacion asteroides, cambio w->win

--- main.cpp
+++ main.cpp
@@ -1,25 +1,32 @@
 #include <SFML/Graphics.hpp>
 #include "Nave.h"
+#include "asteroide.h"
 using namespace sf;
 
 int main(int argc, char *argv[]){
-	RenderWindow w(VideoMode(800,600),"Ejemplo de SFML");
-	w.setFramerateLimit(60);
-	
+	RenderWindow win(VideoMode(640,360),"Asteroid");
+	win.setFramerateLimit(60);
+	asteroide a;
 	Nave navesita(40,3);
-		
-	while(w.isOpen()) {
+
+
+	while(win.isOpen()) {
 		Event e;
-		while(w.pollEvent(e)) {
+		while(win.pollEvent(e)) {
 			if(e.type == Event::Closed)
-				w.close();	
+				win.close();	
 		}
 		
-		w.clear(Color(255,255,255,255));
-		navesita.actualizar();
-		navesita.dibujar(w);
 		
-		w.display();
+		win.clear(Color(40,40,50,255));
+		
+		a.actualizar();
+		navesita.actualizar();
+		navesita.dibujar(win);
+		a.dibujar(win);
+		
+		
+		win.display();
 	}
 	return 0;
 }
