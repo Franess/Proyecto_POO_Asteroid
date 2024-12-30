2c4d7eb202cb7c90549f803ad6a77e0c22b02020
Sun <glassedsun@gmail.com>
2024-12-30 17:46:28 -0300

    a este punto es una reescritura del 90% de la clase, leer ,txt

--- asteroide.cpp
+++ asteroide.cpp
@@ -3,65 +3,77 @@
 #include <cmath>
 #include "complemento_v.h"
 
+#include <iostream>
 using namespace std;
-asteroide::asteroide() {
-	tex.loadFromFile("asteroide.png");
-	spr.setTexture(tex);
-	spr.setOrigin(10,10);
-	spr.setScale(2,2);
+asteroide::asteroide(Texture *tex_asteroide) {
+	//tex=tex_asteroide;
+	//spr.setTexture(*tex);
+	spr.setTexture(*tex_asteroide);
+	spr.setScale(1,1);
 	speed=1;
+	size=1;
+	spr.setOrigin(size*10,size*10);
 }
+
 Vector2f asteroide::posicion(){
 	return spr.getPosition();
 }
+
+void asteroide::r_speed(){
+	speed=static_cast<float>(RNG(150,25)/100.0);
+}
+
+void asteroide::r_size(){
+	size=static_cast<float>(RNG(500,50))/100.0;
+	spr.setScale(size,size);
+	cout<< size<<endl;
+	auto limites=spr.getLocalBounds();
+	spr.setOrigin(limites.width/2,limites.height/2);
+}
+
 void asteroide::reposicionar(){
 	int x=RNG(1), y= RNG(1);
 	if (x>0){
-		x=RNG(640+20,0-20);	
+		x=RNG(640+size*20,0-size*20);	
 		if (y>0){
-			y=360+20;
+			y=360+size*20;
 		}else{
-			y=0;		
+			y=0-size*20;		
 		}
 	}else{
 		if (y>0){
-			x=640+20;
+			x=640+size*20;
 		}else{
-			x=0-35;
+			x=0-size*20;
 		}
-		y=RNG(360+20,0-20);
+		y=RNG(360+size*20,0-size*20);
 	}
 	spr.setPosition(x,y);
 	
 }
-/*void asteroide::cambiar_objetivo(){
-	objetivo.x=RNG(640+64,0-64);
-	objetivo.y=RNG(360+36,0-36);
-}*/
+
 void asteroide::cambiar_objetivo(){
 	int x=RNG(1), y= RNG(1);
 	if (x>0){
-		x=RNG(640+35,0-35);	
+		x=RNG(640+size*32,0-size*32);	
 		if (y>0){
-			y=360+35;
+			y=360+size*32;
 		}else{
-			y=0-35;		
+			y=0-size*32;		
 		}
 	}else{
 		if (y>0){
-			x=640+35;
+			x=640+size*32;
 		}else{
-			x=0-35;
+			x=0-size*32;
 		}
-		y=RNG(360+35,0-35);
+		y=RNG(360+size*32,0-size*32);
 	}
 	objetivo.x=x;
 	objetivo.y=y;
 }
 void asteroide::actualizar(){
 	Vector2f aux=spr.getPosition();
-	aux.x=aux.x;
-	aux.y=aux.y;
 	Vector2f direccion= (objetivo-aux);
 	normal_v(direccion);
 	spr.move(direccion.x*speed,direccion.y*speed);	
--- asteroide.h
+++ asteroide.h
@@ -3,18 +3,23 @@
 #include <SFML/Graphics/Texture.hpp>
 #include <SFML/Graphics/Sprite.hpp>
 #include <SFML/Graphics/RenderWindow.hpp>
+#include <memory>
+using namespace std;
 using namespace sf;
-class asteroide {
-	Texture tex;
+class asteroide{
+	Texture* tex;
 	Sprite spr;
-	Vector2f objetivo;
+	Vector2f objetivo;					//punto hacia el que se dirige el ast
 	float speed;
+	float size;							//valor de covercion 1=20 pixeles
 public:
-	asteroide();
+	asteroide(Texture *tex_asteroide);	//shared_ptr es un puntero inteligente
 	void cambiar_objetivo();
-	void actualizar();
-	void dibujar(RenderWindow &win);
+	void actualizar();					//mueve el ast al objetivo
+	void dibujar(RenderWindow &win);	
 	void reposicionar();
+	void r_speed();
+	void r_size();
 	Vector2f posicion();
 };
 
