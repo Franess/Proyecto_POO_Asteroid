83f278818954e8ced019b043871a70c68f8c081a
Sun <glassedsun@gmail.com>
2024-12-30 17:49:21 -0300

    clase asteroide ahora varia en tamanio, velocidad, punto central y el punto objetivo y de despawneo ahora son dependientes del tamanio

--- main.cpp
+++ main.cpp
@@ -4,41 +4,34 @@
 #include "complemento_v.h"
 #include <SFML/Graphics/Color.hpp>
 #include <vector>
+#include "ast_manip.h"
+#include <SFML/Graphics/Texture.hpp>
 using namespace std;
 using namespace sf;
-
-void respawn(vector<asteroide> &a){
-	for(int i=0;i<a.size();i++) { 
-		if ( (a[i].posicion()> Vector2f(640+10,360+10))or(Vector2f(0-10,0-10)>a[i].posicion()) ){
-			a[i].cambiar_objetivo();
-			a[i].reposicionar();
-		}
-	}
-};
-
 
 int main(int argc, char *argv[]){
 	RenderWindow win(VideoMode((640),(360)),"Asteroid");
 	win.setFramerateLimit(60);
-	vector <asteroide> ast(15);
-	for(int i=0;i<ast.size();i++) { 
-		ast[i].cambiar_objetivo();
-		ast[i].reposicionar();	
-	}
+	Texture* tex_asteroide= new Texture; 			//make_shared es un gestor mas eficiente y seguro que un new Texture, pero en poo dimos new asi que usamo new
+	(*tex_asteroide).loadFromFile("asteroide.png");	//convendria usar shared_ptr para ahorrarnos la eliminacion del puntero
+	vector <asteroide> ast;
 	
 	Nave navesita(40,3);
-	int prueba=0;	//simplemente para probar el sistema de respawn de asteroides;
+	int prueba=0;									//simplemente para probar el sistema de respawn de asteroides;
 
 	while(win.isOpen()) {
 		Event e;
 		while(win.pollEvent(e)) {
-			if(e.type == Event::Closed)
-				win.close();	
+			if(e.type == Event::Closed){
+				delete tex_asteroide;
+				win.close();
+			}
 		}
 		win.clear(Color(40,40,50,255));
 		
 		prueba++;
 		if (prueba>20){
+			spawn(ast,tex_asteroide);
 			respawn(ast);
 			prueba=0;
 		} 
@@ -46,11 +39,12 @@ int main(int argc, char *argv[]){
 			ast[i].actualizar();
 			ast[i].dibujar(win);
 		}
-	navesita.actualizar();
+		navesita.actualizar();
 		navesita.dibujar(win);
 		
 		win.display();
 	}
+	delete tex_asteroide;
 	return 0;
 }
 
