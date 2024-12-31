36d9829446c5980958ca03dee39adaebd7c9e624
Sun <glassedsun@gmail.com>
2024-12-30 21:18:53 -0300

    se añadio la funcion destruir, incompleta por ahora

--- ast_manip.cpp
+++ ast_manip.cpp
@@ -6,7 +6,8 @@ using namespace std;
 
 void spawn (vector<asteroide> &a, Texture* tex){
 	if (a.size()<15){
-		a.push_back(asteroide(tex));
+		int id= a.size()-1;
+		a.push_back(asteroide(tex,id));
 		a[a.size()-1].r_size();
 		a[a.size()-1].r_speed();
 		
@@ -15,8 +16,10 @@ void spawn (vector<asteroide> &a, Texture* tex){
 	}
 }
 void respawn(vector<asteroide> &a){
+	
 	for(int i=0;i<a.size();i++) { 
-		if ( (a[i].posicion()> Vector2f(640+10,360+10))or(Vector2f(0-10,0-10)>a[i].posicion()) ){
+		float limites = a[i].tell_size()*11;
+		if ( (a[i].posicion()> Vector2f(640+limites,360+limites))or(Vector2f(0-limites,0-limites)>a[i].posicion()) ){
 			a[i].r_size();
 			a[i].r_speed();
 			
@@ -26,5 +29,12 @@ void respawn(vector<asteroide> &a){
 		}
 	}
 }
-	
+	void destruir (vector<asteroide> &a, size_t id=-1){
+		if(id!=-1){
+			a[id].r_size();
+			a[id].r_speed();
+			a[id].cambiar_objetivo();
+			a[id].reposicionar();
+		}
+	}
 
