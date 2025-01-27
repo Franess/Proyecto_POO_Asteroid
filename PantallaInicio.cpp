40b47aa520d9ad9994410c71989cffee7681c2b6
Juan Francisco Medina <pokefran1234@gmail.com>
2025-01-27 03:54:36 -0300

    Solucionado area accion de los botones

--- PantallaInicio.cpp
+++ PantallaInicio.cpp
@@ -7,7 +7,7 @@ using namespace std;
 bool calculo_sobreposicion(sf::Vector2f pos_puntero, Boton &b,float ex, float ey)
 {
 	sf::Vector2f pos_actualBoton = b.obtenerVectorPosRelativo();
-	bool zona_x = (pos_actualBoton.x<=pos_puntero.x)*ex && pos_puntero.x<=(pos_actualBoton.x+b.obtenerAnchoBorde())*ex;
+	bool zona_x = (pos_actualBoton.x)*ex <=pos_puntero.x&& pos_puntero.x<=(pos_actualBoton.x+b.obtenerAnchoBorde())*ex;
 	bool zona_y = (pos_actualBoton.y)*ey<=pos_puntero.y && pos_puntero.y<=(pos_actualBoton.y+b.obtenerAltoBorde())*ey;
 	if(zona_x && zona_y) return true;
 	else return false;
