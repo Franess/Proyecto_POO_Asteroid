eefdd6aac6c5b8f41710f9a91638f0fa20e14739
Juan Francisco Medina <pokefran1234@gmail.com>
2025-02-07 22:36:45 -0300

    Se agrega tiempo extra para el cambio de inmunidad (se agrega 1.5 sec)

--- OnePlayer.cpp
+++ OnePlayer.cpp
@@ -210,9 +210,12 @@ void OnePlayer::Actualizar (Juego &j)
 	if((m_navesita.obtenerTiempo()).asMilliseconds()>=3000 && m_navesita.obtenerColision()){
 		m_navesita.cambiarColision();
 		m_navesita.cambiarTransparencia();
-		m_navesita.cambiarInmunidad();
 		m_navesita.respawn();
 		reproducir(m_buffer[3],m_sound);
+	}
+	if((m_navesita.obtenerTiempo()).asMilliseconds()>=4500)
+	{
+		m_navesita.cambiarInmunidad();
 	}
 	for(Boton &x:vec_botones) x.actualizar();
 	Vector2f vecPos_correccion = correccionPosicionNave(m_navesita);
