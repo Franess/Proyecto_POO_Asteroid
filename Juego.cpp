882e5675d7c80546857a1acc8adccedde2eeaf10
Sun <glassedsun@gmail.com>
2025-02-08 18:23:26 -0300

    empezada la implementacion de F11

--- Juego.cpp
+++ Juego.cpp
@@ -4,9 +4,10 @@
 
 using namespace std;
 
-Juego::Juego(Escena* e):m_win(sf::VideoMode((640),(360)),"Asteroid") {
+Juego::Juego(Escena* e):m_win(sf::VideoMode((640),(360)),"Asteroid"),vista(sf::FloatRect(0,0,640,360)){
 	m_win.setFramerateLimit(60);
 	m_escenaActual=e;
+	pantalla_completa=0;
 }
 Juego::~Juego()
 {
@@ -25,6 +26,23 @@ void Juego::jugar()
 			}else
 			{
 				m_escenaActual->ProcesarEvento(*this,e);
+				if(e.type == sf::Event::KeyReleased && e.key.code == sf::Keyboard::F11)
+				{
+					pantalla_completa = !pantalla_completa;
+					if(pantalla_completa==0){
+						
+						
+					}else{
+						sf::VideoMode aux = sf::VideoMode::getDesktopMode();
+						
+						vista.setSize(aux.width,aux.height);
+						m_win.create(aux,"Asteroid",sf::Style::Fullscreen);
+						cout<<aux.height;
+						vista.zoom(1.f/(aux.height/360));
+						m_win.setView(vista);
+						
+					}
+				}
 			}
 		}
 		m_win.clear(sf::Color(0,0,0,255));
@@ -44,3 +62,4 @@ void Juego::terminar()
 {
 	m_win.close();
 }
+
