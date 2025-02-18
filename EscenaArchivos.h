65759f457a3f2294522800b2a34555a602280e2e
Juan Francisco Medina <pokefran1234@gmail.com>
2025-02-17 22:15:55 -0300

    Se implementa la parte grafica de la escena de control de archivos

--- EscenaArchivos.h
+++ EscenaArchivos.h
@@ -1,6 +1,12 @@
 #ifndef ESCENAARCHIVOS_H
 #define ESCENAARCHIVOS_H
+#include "funcionesBotones.h"
+#include <stdexcept>
+#include <fstream>
 #include "Escena.h"
+#include "Settings.h"
+#include "Boton.h"
+#include "PantallaInicio.h"
 
 class EscenaArchivos : public Escena {
 public:
@@ -10,9 +16,15 @@ public:
 	void ProcesarEvento (Juego & j, sf::Event e)override;
 	~EscenaArchivos()override;
 private:
-	sf::Text m_tituloPantalla;
+	sf::Text m_tituloPantalla, m_indicadorArchivo, m_indicadorEstado;
 	sf::RectangleShape m_seccionTitulo;
 	sf::Font m_fuente;
+	Settings m_settings;
+	std::vector<bool> m_estadosArchivos;
+	std::vector<Boton> m_vecbotones;
+	std::vector<sf::Text> m_textosEstados;
+	vector<float> m_escalas;
+	int contador_estados;
 };
 
 #endif
